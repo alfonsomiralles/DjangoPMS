e03f002359b561387d786bfe3b6f5bb01e9f5b7c
Fonss <alfonsomiralles1@gmail.com>
2022-12-17 19:08:55 +0100

    feature:users can view and delete their own accommodations

---
+++ accommodation/forms.py
@@ -0,0 +1,9 @@
+from django.forms import ModelForm
+from .models import Accommodation
+
+class AccommodationForm(ModelForm):
+    class Meta:
+        model = Accommodation
+        fields = '__all__'
+        #exclude = ('date',)
+        #widgets = {'estimated_end': DateInput(attrs={'type':'date'}),}
--- users/forms.py
+++ users/forms.py
@@ -1,16 +1,18 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import SetPasswordForm
 from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import SetPasswordForm
+
 
 class UpdateUserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ('username','first_name','last_name','email',)
+        fields = ('username', 'first_name', 'last_name', 'email',)
         #fields = '__all__'
         #exclude = ('password','last_login','is_superuser', 'is_staff','is_active','date_joined')
 
-class SetPasswordForm(SetPasswordForm):
+
+class MySetPasswordForm(SetPasswordForm):
     class Meta:
         model = get_user_model()
-        fields = ['new_password1', 'new_password2']        
+        fields = ['new_password1', 'new_password2']
